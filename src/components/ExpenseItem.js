03aa4f279f875671e08a9ef4fbdf16240fe64a54
SofiaMEweiss <sofia.weiss@iths.se>
2021-05-04 11:11:30 +0200

    Added CSS to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,19 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-	return 
-	<h2>Expense item!</h2>
+	return (
+	<div className="expense-item">
+		<div>
+			Date
+		</div>
+		<div className="expense-item__description">
+			<h2> Title</h2>
+			<div className="expense-item__price"> 
+				Amount
+			</div>
+		</div>
+	</div>
+	);
 }
 
 export default ExpenseItem;
